b8e88230c9ab86cb8581ef42ce7b52a17f65608e
Tolga Yıldız <tolgayildiz91@hotmail.com.tr>
2023-05-03 18:24:40 +0300

    UI değişiklikleri yapıldı. view componentler eklendi

---
+++ TraversalCoreProje/ViewComponents/Default/_Testimonial.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _Testimonial:ViewComponent
+    {
+        TestimonialManager _testimonialManager = new TestimonialManager(new EfTestimonialDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = _testimonialManager.TGetList();
+            return View(values);
+        }
+    }
+}
