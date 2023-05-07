6a3fc6efe04acb4f65838d870ed942478a9784e1
Tolga Yıldız <tolgayildiz91@hotmail.com.tr>
2023-05-08 00:34:45 +0300

    reservasyon altyapı

--- DataAccessLayer/Migrations/20230507212807_mig_add_reservation_entity.Designer.cs
+++ DataAccessLayer/Migrations/20230507212807_mig_add_reservation_entity.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230507212807_mig_add_reservation_entity")]
+    partial class mig_add_reservation_entity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -407,7 +409,7 @@ namespace DataAccessLayer.Migrations
 
                     b.HasIndex("AppUserId");
 
-                    b.ToTable("Reservations");
+                    b.ToTable("Reservation");
                 });
 
             modelBuilder.Entity("EntityLayer.Concrete.SubAbout", b =>
---
+++ TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
@@ -0,0 +1,33 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    public class ReservationController : Controller
+    {
+        [Area("Member")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MyCurrentReservation()
+        {
+            return View();
+        }
+        public IActionResult MyOldReservation()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NewReservation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewReservation(Reservation p)
+        {
+            return View();
+        }
+    }
+}
