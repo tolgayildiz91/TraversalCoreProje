992cb291ab2169213cb486ed039f2b39c472aa32
Tolga Yıldız <tolgayildiz91@hotmail.com.tr>
2023-05-08 18:24:24 +0300

    profil işlemleri yapıldı.

---
+++ TraversalCoreProje/ViewComponents/MemberDashboard/_PlatformSetting.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.ViewComponents.MemberDashboard
+{
+    public class _PlatformSetting:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+           
+            return View();
+        }
+    }
+}
